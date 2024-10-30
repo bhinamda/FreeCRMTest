6347e9b1758fa1b954f1a4443064df0b296d64b0
Bhakti Inamdar <bhaktiinamdar2212@gmail.com>
2024-10-30 10:52:45 -0400

    Added WebEventListener to generate selenium action logs

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -1,9 +1,11 @@
 package com.crm.qa.base;
 import com.crm.qa.util.TestUtils;
+import com.crm.qa.util.WebEventListener;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,6 +17,8 @@ public class TestBase
     public static WebDriver driver;
     public static Properties prop;
     public static String projectPath= System.getProperty("user.dir");
+    public static EventFiringWebDriver e_driver;
+    public static  WebEventListener eventListener;
 
     public TestBase()
     {
@@ -47,6 +51,13 @@ public class TestBase
             WebDriverManager.firefoxdriver().setup();
             driver=new FirefoxDriver();
         }
+
+        e_driver=new EventFiringWebDriver(driver);
+        //register the event listener handle(WebEventListener class object) with the EventFiringWebDriver
+        eventListener=new WebEventListener();
+        e_driver.register(eventListener);
+        driver=e_driver;
+
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(TestUtils.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
